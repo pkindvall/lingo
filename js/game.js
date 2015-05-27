ac8dcb6eb48881ef22a232e8c257b45702d96ca9
patrik <patrikkindvall@gmail.com>
2015-05-27 13:31:13 +0200

    ingnting

--- js/game.js
+++ js/game.js
@@ -5,11 +5,31 @@ var valtOrd = ordLista[Math.floor (Math.random()*3)];
 console.log(valtOrd);
 
 var button = document.getElementsByTagName("button")[0];
-button.addEventListener("click", function()){
+button.addEventListener("click", function(){
 
 	var användarOrd = document.getElementsByTagName("input")[0].value;
 
-	if(value === användarOrd)
+	//hämta rutor och lägg in bokstäverna rätt
+
+	var rutor = document.getElementsByTagName("td");
+
+	for (var i=0;i<5;i++)
+{
+	rutor[i].innerHTML = anvandarOrd[i];
+
+	if (användarOrd)[i] === valtOrd[i]){
+
+	}
+rutor[i].style.background ="green";
+	}
+	else
+	{
+	rutor[i].style.background = "red";
+}
+}
+
+
+	if(valtOrd === användarOrd)
 	{
 		alert ("Grattis");
 	}else 
@@ -17,4 +37,7 @@ button.addEventListener("click", function()){
 		alert("Obs! försöks igen");
 	}
 }
+
+
+
 
